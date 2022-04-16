20a15a9ed43b355958dc0fe598d4141561f47ab3
VijayaprakashVp <prakashvijay905@gmail.com>
2022-04-16 18:22:13 +0530

    Initial Commit

---
+++ react-city-country/src/Components/AddCity.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AddCity = () => {
+  return <div>AddCity</div>;
+};
---
+++ react-city-country/src/Components/AddCountry.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AddCountry = () => {
+  return <div>AddCountry</div>;
+};
